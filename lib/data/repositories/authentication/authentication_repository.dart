5a9760fc1c3f6a9dd24d30e3b639748bb3f9a380
Camilo Tello <camilotello04@gmail.com>
2025-02-21 15:42:20 -0500

    firebase added

---
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class AuthencationRepository extends GetxController {
+  static AuthencationRepository get to => Get.find();
+
+  // variables
+  final deviceStorage = GetStorage();
+
+  // called from main.dart on app start
+  @override
+  void onReady() {
+    FlutterNativeSplash.remove();
+    screenRedirect();
+  }
+
+  // fuction to show Relevant Screen
+  screenRedirect() {
+    if (deviceStorage.read('isFirstTime') == null) {
+      Get.to('/onboarding');
+    } else {
+      Get.offAllNamed('/login');
+    }
+  }
+
+
+
+
+
+}
